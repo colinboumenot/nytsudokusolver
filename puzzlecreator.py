6dd98e4b76bb1ddf6cdf17b5b4570d1945f95ac0
Christopher Boumenot <boumenot@gmail.com>
2023-02-21 19:31:46 -0800

    Solver and PuzzleCreator implemented

--- puzzlecreator.py
+++ puzzlecreator.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import List
 import math
 import random
+from solver import Solver
 
 class PuzzleCreator:
 
@@ -63,8 +64,18 @@ class PuzzleCreator:
             print()
 
 p = PuzzleCreator(1,9)
+s = Solver()
 p.generate_puzzle()
 p.print_puzzle()
+print()
+result = s.solve(p.puzzle)
+if result is None:
+    print("L")
+else:
+    for x in range(9):
+        for y in range(9):
+            print(result[x][y], end = " ")
+        print()
 
 
 
